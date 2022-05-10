914ac58b68d378ad1630b25a6d8c25f32f73c3e0
HarpaxPantensus <kaloglu.oktay@gmail.com>
2022-05-10 19:45:22 +0300

    aptain's log, Stardate 5631.7. We have been searching a solution for a while. Cmdr. Riker and I have decied to make changes in the logic. we are rolling back the queens so we can start our serach on a solid gouround. The queens shall rewind.

--- python/main.py
+++ python/main.py
@@ -1,22 +1,6 @@
 import random as rnd
 # Gradient Descent approach is chosen to solve The Eight Queens problem
 
-def viewMatrix(table):
-    rowFiller=" -- "
-    columnFiller="|"
-    twoD=table
-    tableLen=len(table)
-    for i in range(tableLen-1):
-        for j in range(tableLen-1):
-            print(twoD[i][j],end=rowFiller)
-        print(twoD[i][tableLen-1])
-        for j in range(tableLen-1):
-            print(columnFiller+"    ",end="")
-        print(columnFiller)
-    for j in range(tableLen-1):
-        print(twoD[tableLen-1][j],end=rowFiller)
-    print(twoD[tableLen-1][tableLen-1])
-
 #The chess table has 8 rows, 8 columns and total of 64 squares
 #I am using the rows as an array and store the columns as the data
 
@@ -60,73 +44,26 @@ def rndStart(table):
         table[i]=iniPos[randomIndex]
         iniPos.remove(iniPos[randomIndex])
 
-
-#calculate the collisions at the squares that caused by the queens
-#return the collision matrix
-#necessary calculations made by the help of breadth first approach
-def calculateTheMatrix(table):
-    numberOfQueens=len(table)
-    collisionMatrix=[]
-    for i in range(numberOfQueens):
-        collisionMatrix.append([0]*numberOfQueens)
-    for i in range(numberOfQueens):
-        #i specifies the column
-        # add a collision to the squares that the queen can reach
-
-        collisionMatrix[table[i]][i]+=1 #for the starting point
-
-        for j in range(1,numberOfQueens):#needs improvement for this loop count
-            #for lateral collision from starting position
-            if i+j<numberOfQueens:
-                collisionMatrix[table[i]][i+j]+=1 #add collisions after the starting position's  at x axis
-            if i-j>-1:
-                collisionMatrix[table[i]][i-j]+=1 #add collisions before the starting position's  at x axis
-
-            if table[i]-j>-1:
-                collisionMatrix[table[i]-j][i]+=1 #add collisions above the starting position's  at y axis
-                if i-j>-1:
-                    collisionMatrix[table[i]-j][i-j]+=1 #add collisions to the starting position above and left at y axis and x axis
-
-                if i+j<numberOfQueens:
-                    collisionMatrix[table[i]-j][i+j]+=1 #add collisions to the starting position above and right at y axis and x axis
-            if table[i]+j<numberOfQueens:
-                collisionMatrix[table[i]+j][i]+=1 #add collisions below the starting position's at y axis
-                if i-j>-1:
-                    collisionMatrix[table[i]+j][i-j]+=1 #add collisions to the starting position above and left at y axis and x axis
-
-                if i+j<numberOfQueens:
-                    collisionMatrix[table[i]+j][i+j]+=1 #add collisions to the starting position above and right at y axis and x axis
+# a function that calculates the current state's collisions
+def calculateCollision(table):
 
 
-    return collisionMatrix
 
-def gradientDecent(table):
-    nQueens=len(table)
-    while(True):
-        collisionMatrix=calculateTheMatrix(table)
 
-        while(True):
-            arandomQueen=rnd.randint(0,nQueens)
-            queensCurrenCollision=collisionMatrix[arandomQueen][table[arandomQueen]]
-            coloumnsCollisions=[]
-            for i in range(0,nQueens):
-                coloumnsCollisions.append([collisionMatrix[i][table[arandomQueen]],i])#number of collision,column index
-            while(len(coloumnsCollisions)>0):
-                randNum=rnd.randint(0,len(coloumnsCollisions))
-                if(coloumnsCollisions[randNum][0]<queensCurrenCollision):
-                    table[arandomQueen]=randNum
-                    break
+# a function that gets 2 random queens and it checks whether new state is better than current one
+    # and swaps the queens positions as new positions.
+    # if the current table has zero collisions that is a solution
+    # if no queen can swap
 
-        viewMatrix(collisionMatrix)
 
 
 def main():
-    numberOfqueens=4
+    numberOfqueens=5
     table= [0]*numberOfqueens
     rndStart(table)
-    table=[3,0,2,1]
-    gradientDecent(table)
-    viewTable(table)
+
+
+
 
 
 if __name__ == '__main__':
