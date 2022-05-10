900cd8b75b222d75bffc5b6767d853e9ffe0838c
HarpaxPantensus <kaloglu.oktay@gmail.com>
2022-05-10 04:26:37 +0300

    Suplamentery, I made sure that the queens initialized at different ro…

--- python/main.py
+++ python/main.py
@@ -1,6 +1,21 @@
 import random as rnd
 # Gradient Descent approach is chosen to solve The Eight Queens problem
 
+def viewMatrix(table):
+    rowFiller=" -- "
+    columnFiller="|"
+    twoD=table
+    tableLen=len(table)
+    for i in range(tableLen-1):
+        for j in range(tableLen-1):
+            print(twoD[i][j],end=rowFiller)
+        print(twoD[i][tableLen-1])
+        for j in range(tableLen-1):
+            print(columnFiller+"    ",end="")
+        print(columnFiller)
+    for j in range(tableLen-1):
+        print(twoD[tableLen-1][j],end=rowFiller)
+    print(twoD[tableLen-1][tableLen-1])
 
 #The chess table has 8 rows, 8 columns and total of 64 squares
 #I am using the rows as an array and store the columns as the data
@@ -8,7 +23,7 @@ import random as rnd
 #make a new two dimensional array from one dimensional array
 def arrayConverter(table):
     convertedArray=[]
-    for i in range(8):
+    for i in range(len(table)):
         emptyArray=["O"]*8
         emptyArray[table[i]]="X"
         convertedArray.append(emptyArray)
@@ -28,27 +43,89 @@ def viewTable(table):
         for j in range(tableLen-1):
             print(columnFiller+"    ",end="")
         print(columnFiller)
-    for j in range(7):
+    for j in range(tableLen-1):
         print(twoD[j][tableLen-1]+rowFiller,end="")
     print(twoD[tableLen-1][tableLen-1])
 
 # randomly assigning queens positions
 #this method insures the queens wont be in the same row at the initial state
 def rndStart(table):
-    iniPos=[0,1,2,3,4,5,6,7]
+    iniPos=[]
+
     tableLen=len(table)
+    for i in range(tableLen):
+        iniPos.append(i)
     for i in range(tableLen):
         randomIndex=rnd.randint(0,len(iniPos)-1)
         table[i]=iniPos[randomIndex]
         iniPos.remove(iniPos[randomIndex])
 
+
+#calculate the collisions at the squares that caused by the queens
+#return the collision matrix
+#necessary calculations made by the help of breadth first approach
 def calculateTheMatrix(table):
-    i=1
+    numberOfQueens=len(table)
+    collisionMatrix=[]
+    for i in range(numberOfQueens):
+        collisionMatrix.append([0]*numberOfQueens)
+    for i in range(numberOfQueens):
+        #i specifies the column
+        # add a collision to the squares that the queen can reach
+
+        collisionMatrix[table[i]][i]+=1 #for the starting point
+
+        for j in range(1,numberOfQueens):#needs improvement for this loop count
+            #for lateral collision from starting position
+            if i+j<numberOfQueens:
+                collisionMatrix[table[i]][i+j]+=1 #add collisions after the starting position's  at x axis
+            if i-j>-1:
+                collisionMatrix[table[i]][i-j]+=1 #add collisions before the starting position's  at x axis
+
+            if table[i]-j>-1:
+                collisionMatrix[table[i]-j][i]+=1 #add collisions above the starting position's  at y axis
+                if i-j>-1:
+                    collisionMatrix[table[i]-j][i-j]+=1 #add collisions to the starting position above and left at y axis and x axis
+
+                if i+j<numberOfQueens:
+                    collisionMatrix[table[i]-j][i+j]+=1 #add collisions to the starting position above and right at y axis and x axis
+            if table[i]+j<numberOfQueens:
+                collisionMatrix[table[i]+j][i]+=1 #add collisions below the starting position's at y axis
+                if i-j>-1:
+                    collisionMatrix[table[i]+j][i-j]+=1 #add collisions to the starting position above and left at y axis and x axis
+
+                if i+j<numberOfQueens:
+                    collisionMatrix[table[i]+j][i+j]+=1 #add collisions to the starting position above and right at y axis and x axis
+
+
+    return collisionMatrix
+
+def gradientDecent(table):
+    nQueens=len(table)
+    while(True):
+        collisionMatrix=calculateTheMatrix(table)
+
+        while(True):
+            arandomQueen=rnd.randint(0,nQueens)
+            queensCurrenCollision=collisionMatrix[arandomQueen][table[arandomQueen]]
+            coloumnsCollisions=[]
+            for i in range(0,nQueens):
+                coloumnsCollisions.append([collisionMatrix[i][table[arandomQueen]],i])#number of collision,column index
+            while(len(coloumnsCollisions)>0):
+                randNum=rnd.randint(0,len(coloumnsCollisions))
+                if(coloumnsCollisions[randNum][0]<queensCurrenCollision):
+                    table[arandomQueen]=randNum
+                    break
+
+        viewMatrix(collisionMatrix)
+
 
 def main():
-    numberOfqueens=8
+    numberOfqueens=4
     table= [0]*numberOfqueens
     rndStart(table)
+    table=[3,0,2,1]
+    gradientDecent(table)
     viewTable(table)
 
 
